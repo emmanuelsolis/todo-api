3bb451416337ffa6aef2fdb69ccb33f131b20d56
Emmanuel Solis <emma.ruso@outlook.com>
2023-06-24 21:56:52 -0600

    configuramos el entorno y añadimos librerias

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from "http";
+import bodyParser from 'body-parser';
+import cookieParser from  'cookie-parser'
+import cors from 'cors';
+import compression from 'compression'
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}))
+
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json());
+
+
+const  server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server is running on port 8080');
+})
+
