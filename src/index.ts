b35332521b055ff7d48ff313fabe397ccc37e8ba
Emmanuel Solis <emma.ruso@outlook.com>
2023-06-26 14:57:04 -0600

    Creamos nuetsro primer controller, e inicamos el CRUD creando nuestro primer usuario y verificando la Correcta instegración de Mongo DB ATlas

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from  'cookie-parser'
 import cors from 'cors';
 import compression from 'compression'
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -24,7 +25,9 @@ server.listen(8080, () => {
     console.log('Server is running on port 8080');
 })
 
-const MONGO_URL = 'mongodb+srv://Admin:WjYlqQvFPgwNCQf1@cluster1.chodwcu.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = 'mongodb+srv://Admin:WjYlqQvFPgwNCQf1@cluster1.chodwcu.mongodb.net/Todo-API?retryWrites=true&w=majority'
 mongoose.Promise = Promise
 mongoose.connect(MONGO_URL)
 mongoose.connection.on('error', (error: Error) => console.log(error))
+
+app.use('/', router())
